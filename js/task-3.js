6778f8a2f43873d0a03ba61d4aa95d6441e10fc3
Eraserstreet <148534901+Eraserstreet@users.noreply.github.com>
2024-02-03 19:07:27 +0200

    Home-JS-4-0.1

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,7 @@
-function filterArray(numbers, value) {
-  let collectorArray = [];
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > value) {
-      collectorArray.push(numbers[i]);
-    }
-  }
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-  return collectorArray;
-}
-
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
